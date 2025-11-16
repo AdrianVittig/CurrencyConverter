00cf444a3fdc98a5201cd732a2a9da8f6f80e35f
AdrianVittig <adrianvitig@gmail.com>
2025-11-16 13:54:38 +0200

    properties

--- CurrencyConverter/Currency.cs
+++ CurrencyConverter/Currency.cs
@@ -9,9 +9,9 @@ namespace CurrencyConverter
     // Class representing a currency with its code, name, and exchange rate to USD
     internal class Currency
     {
-        public string Code { get; set; }
-        public string Name { get; set;  }
-        public decimal RateToUSD { get; set; }
+        private string Code { get; set; }
+        private string Name { get; set;  }
+        private decimal RateToUSD { get; set; }
 
         // Constructor to initialize currency properties
         public Currency(string code, string name, decimal rateToUSD)
@@ -20,7 +20,6 @@ namespace CurrencyConverter
             Name = name;
             RateToUSD = rateToUSD;
         }
-
         
         public override string ToString()
         {
